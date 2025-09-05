8de20b651ba2442588eed81d0bec505042562e50
darwinangelo305 <angelodarwin305@gmail.com>
2025-09-05 13:20:05 -0300

    eu não sei oq eu errei
    desculpa mais eu n sei oq eu errei no codigo eu copiei exatamente oq tava na tela e  o window de erro

---
+++ aula1/condicions(exercicio)/exerciciopart2.html/expart3/script.js
@@ -0,0 +1,16 @@
+function verificar () {
+    var data = new Date
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('divBres')
+    if (fano.ariaValueMax.length == 0 || Number(fano.value) > {
+        window.alert("[ERRO] Verifique os dados e tente novamente")
+    } else {
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+    }
+    )
+}
+
+/* EU TENTEI EU TENTEI MUITO EU NÂO SEI OQ EU
+ERREI SE EU PAUSASE IRIA DEMORAR 2 HORAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS*/
