22a7f20ebec26723db02b497b0934f48f8e2af60
darwinangelo305 <angelodarwin305@gmail.com>
2025-09-20 14:41:11 -0300

    aq
    ta aqui a pagina q o senhor pediu

--- exercicio/pagina/pag.js
+++ exercicio/pagina/pag.js
@@ -1,8 +1,13 @@
 function pagina() {
-    let opp = document.getElementById('nam')
-    if (opp = 11) {
-        document.log('CORRETO')
-    } else {
-        document.log('INCORRETO')
+  
+    let tex = document.getElementById('tst')
+
+    console.log(`Ola ${opp}`)
+    console.log(`Aqui diz que sua idade e ${num}`)
+    if (num >= 18) {
+        window.alert('Voce e maior de idade!')
+
+        window.alert('Voce e menor de idade!')
+        c++
     }
 }
