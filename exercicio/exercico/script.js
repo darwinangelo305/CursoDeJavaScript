4f27695a440ee10942b0348b35ffa60bacbdc799
darwinangelo305 <angelodarwin305@gmail.com>
2025-09-23 13:09:46 -0300

    AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
    ULTIMO EXERCICIO:
        essa foi a parte dois do ultimo video
        onde eu finaliso o codigu

--- exercicio/exercico/script.js
+++ exercicio/exercico/script.js
@@ -22,8 +22,37 @@ function iLista(n, l) {
 
 function adicionar(){
     if(isNumero(num.value) && linLista(num.value)) {
-        window.alert('Tudo OK')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
     } else {
         window.alert('valor invalido ou ja encontrado na lista')
+    }
+    num.value = ' '
+    num.focus()
+}
+
+function finalisar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalisar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (falores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ' '
+        res.innerHTML += `<p>Ao todo, teos ${tot} numeros cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`    
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`        
+        res.innerHTML += `<p>Somando todos os valores temos ${soma}.</p>`    
+        res.innerHTML += `<p>A media dos valores digitados e ${media}.</p>`    
+        
     }
 }
